c7601fec42ee3a70b25c1bcbe0e23bcdb2a67ab1
Eugene White <eugenewhite1994@gmail.com>
2020-07-11 19:10:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,8 @@
 def reverse_each_word(word)
-  
+    reversed = string.split("")
+  reversedArray = []
+  word.size.times { reversed << reversed.pop }
+  reversedArray.join
+end
+puts reverse_string("hello")
   
